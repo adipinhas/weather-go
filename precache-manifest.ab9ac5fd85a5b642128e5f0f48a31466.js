c02514e36b1a02cd569991dd040c151e9a65d388
adipinhas <adi9247@gmail.com>
2020-03-27 13:37:38 +0300

    Updates

---
+++ precache-manifest.ab9ac5fd85a5b642128e5f0f48a31466.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce58ca2dddc41a0b1ac7c6ce9471e1e5",
+    "url": "/weather-go/index.html"
+  },
+  {
+    "revision": "94818b5e47d5661b21bc",
+    "url": "/weather-go/static/css/main.68152fae.chunk.css"
+  },
+  {
+    "revision": "ce4676587acd56b56aff",
+    "url": "/weather-go/static/js/2.b933dc82.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/weather-go/static/js/2.b933dc82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94818b5e47d5661b21bc",
+    "url": "/weather-go/static/js/main.fec64df4.chunk.js"
+  },
+  {
+    "revision": "1877a273e3e87976136d",
+    "url": "/weather-go/static/js/runtime-main.2f1e50ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-go/static/media/logo.5d5d9eef.svg"
+  }
+]);
